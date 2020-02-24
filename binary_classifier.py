ebd763bcfdfece701de30165db63f16cab1a5188
Mehrdad Nafisi <thrnafism@gmail.com>
2020-02-24 12:15:30 -0800

    initial commit

---
+++ binary_classifier.py
@@ -0,0 +1,9 @@
+import sklearn.datasets
+import matplotlib.pyplot as plt
+import torch
+
+X,y = sklearn.datasets.make_moons(500,noise=0.1)
+plt.scatter(X[:,0],X[:,1],s=40,c=y)
+plt.show()
+plt.savefig('myfig.png' )
+
