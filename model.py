02586f433e015c93de0f0b67edd0697e74f9a806
Mehrdad Nafisi <thrnafism@gmail.com>
2020-02-24 17:51:30 -0800

    define a basin nn with 2 hidden layers
    it is a basic designe to start with

--- model.py
+++ model.py
@@ -1,18 +1,10 @@
 import torch
-import sklearn.datasets
+
 import matplotlib.pyplot as plt
 
 import torch.nn as nn
 import torch.nn.functional as F
 
-X,y = sklearn.datasets.make_moons(500,noise=0.1)
-plt.scatter(X[:,0],X[:,1],s=40,c=y)
-#plt.show()
-#plt.savefig('myfig.png' )
-
-# numpy to tensor
-X = torch.from_numpy(X).type(torch.FloatTensor)
-y = torch.from_numpy(y).type(torch.LongTensor)
 
 
 class Net(nn.Module):
